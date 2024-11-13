52f9fc88b0373b72dc87bd3d05bb68fefe2f68cb
“Eden” <3188483681@qq.com>
2024-11-13 09:39:33 +0800

    更新权限设置界面，未连接主界面

--- SMS/FormRoleRight.Designer.cs
+++ SMS/FormRoleRight.Designer.cs
@@ -29,12 +29,102 @@ namespace SMS
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("节点5");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("节点6");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("节点2", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("节点3");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("节点4");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("节点0", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("节点1");
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonCommit = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonCancel);
+            this.panel1.Controls.Add(this.buttonCommit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 608);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(404, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonCommit
+            // 
+            this.buttonCommit.Location = new System.Drawing.Point(97, 23);
+            this.buttonCommit.Name = "buttonCommit";
+            this.buttonCommit.Size = new System.Drawing.Size(81, 35);
+            this.buttonCommit.TabIndex = 0;
+            this.buttonCommit.Text = "提交";
+            this.buttonCommit.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(304, 23);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(81, 35);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "取消";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // treeView1
+            // 
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Indent = 24;
+            this.treeView1.ItemHeight = 20;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "节点5";
+            treeNode1.Text = "节点5";
+            treeNode2.Name = "节点6";
+            treeNode2.Text = "节点6";
+            treeNode3.Name = "节点2";
+            treeNode3.Text = "节点2";
+            treeNode4.Name = "节点3";
+            treeNode4.Text = "节点3";
+            treeNode5.Name = "节点4";
+            treeNode5.Text = "节点4";
+            treeNode6.Name = "节点0";
+            treeNode6.Text = "节点0";
+            treeNode7.Name = "节点1";
+            treeNode7.Text = "节点1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7});
+            this.treeView1.Size = new System.Drawing.Size(404, 608);
+            this.treeView1.TabIndex = 1;
+            // 
+            // FormRoleRight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(404, 691);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormRoleRight";
             this.Text = "FormRoleRight";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonCommit;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
